d63773291f8b589dc9712ffa8862d9d1f9e063d4
Carlos-Paz22 <pazaupazcarlossebastian@gmail.com>
2022-06-09 16:41:52 -0500

    prueba

--- src/__init__.py
+++ src/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template
 import os
 app = Flask(__name__, template_folder= 'views')
 app.secret_key ="crack"
-#importar los controllers
+
 from src.controllers import *
 
 
