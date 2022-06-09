e9dad2476756fce179d320b03c86aeca2976b8c0
Carlos-Paz22 <pazaupazcarlossebastian@gmail.com>
2022-06-09 15:58:29 -0500

    'carga-heroku

--- app.py
+++ app.py
@@ -1,9 +1,8 @@
 from src import create_app
 
-if __name__ == '__main__':
-    create_app() 
+#if __name__ == '__main__':
+#    create_app() 
 
 
-#heroku
-#from src import create_app
-#app=  create_app()
+from src import create_app
+app=  create_app()
